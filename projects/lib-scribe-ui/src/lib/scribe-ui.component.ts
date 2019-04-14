ebfba84a771a830154e5bf5079ac6243e317bf4f
roka51 <robert.kaczmarek@pf.pl>
2019-04-14 12:50:39 +0200

    add bootstrap 4.3

---
+++ projects/lib-scribe-ui/src/lib/scribe-ui.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sc-ui-scribe',
+  template: `
+    <p>
+      scribe-ui works!
+    </p>
+  `,
+  styles: []
+})
+export class ScribeUiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/lib-scribe-ui/src/lib/scribe-ui.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ScribeUiComponent } from './scribe-ui.component';
+
+@NgModule({
+  declarations: [ScribeUiComponent],
+  imports: [
+  ],
+  exports: [ScribeUiComponent]
+})
+export class ScribeUiModule { }
