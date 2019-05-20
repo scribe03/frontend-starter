f0e7f896fcf7918392d16053086d02899c5cb152
roka51 <robert.kaczmarek@pf.pl>
2019-05-20 20:52:37 +0200

    css

--- src/app/ui/form/form-group/form-group-label.animation.ts
+++ src/app/ui/form/form-group/form-group-label.animation.ts
@@ -3,12 +3,12 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 export const labelUpDown = trigger('labelUpDown', [
     state('void', style({position: 'absolute', left: '4px', top: '-4px', fontSize: '12px'})),
     state('up', style({position: 'absolute', left: '4px', top: '-4px', fontSize: '12px'})),
-    state('down', style({position: 'absolute', left: '4px', top: '6px', fontSize: '16px'})),
+    state('down', style({position: 'absolute', left: '4px', top: '6px', fontSize: '18px'})),
     transition('* => up', [
         style({position: 'absolute', left: 0, top: 0}),
         animate('0.25s linear', style({transform: 'translateY(-4px)', left: '4px', fontSize: '12px'}))
     ]),
     transition('* => down', [
-        animate('0.25s linear', style({transform: 'translateY(12px)', fontSize: '16px'}))
+        animate('0.25s linear', style({transform: 'translateY(12px)', fontSize: '18px'}))
     ])
 ]);
