745019a3117b034800f7f6f4029ef0c7b39c5ed1
roka51 <robert.kaczmarek@pf.pl>
2019-08-25 13:57:49 +0200

    add selectors to lib-scribe

--- projects/lib-scribe/src/lib/operators/repetitions.ts
+++ projects/lib-scribe/src/lib/operators/repetitions.ts
@@ -1,13 +1,13 @@
 import { MonoTypeOperatorFunction, Observable, of } from 'rxjs';
 import { delay, expand, takeWhile } from 'rxjs/operators';
 
-function repetitions<T>(delayTime: number, isRepetitionFn: (result: T) => boolean): MonoTypeOperatorFunction<T> {
+function repetitions<T>(delayTime: number, isStopRepetitionFn: (result: T) => boolean): MonoTypeOperatorFunction<T> {
   let isNext = true;
 
   return (input$: Observable<T>) =>
     input$.pipe(
       expand((result: T) => {
-        if (!isRepetitionFn(result)) {
+        if (!isStopRepetitionFn(result) && isNext) {
           return input$.pipe(delay(delayTime));
         }
 
